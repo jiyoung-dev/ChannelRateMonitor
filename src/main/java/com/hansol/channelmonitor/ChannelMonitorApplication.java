d5ab48d2e9cd802f5de717d784c9fa470559b901
jykim0317 <jykim0317@hansol.com>
2024-08-21 17:58:31 +0900

    Java Timer class 변경 - 시스템시간 변경시, 타이머 정확성이 떨어지는 이슈가 존재함. - 대체 클래스: ScheduledExecutorService 를 사용하면 서버시간에 관계없이 동작.

--- src/main/java/com/hansol/channelmonitor/ChannelMonitorApplication.java
+++ src/main/java/com/hansol/channelmonitor/ChannelMonitorApplication.java
@@ -3,8 +3,9 @@ package com.hansol.channelmonitor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ChannelMonitorApplication {
     private static final Logger logger = LogManager.getLogger(ChannelMonitorApplication.class);
@@ -13,13 +14,14 @@ public class ChannelMonitorApplication {
         logger.info("Starting ChannelMonitorApplication...");
 
         MonitorService monitorService = new MonitorService();
-
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                monitorService.monitorBusyRate();
-            }
-        }, 0, 15000);
+        
+        Runnable runnable = new Runnable() {
+        	  @Override 
+        	  public void run() {
+        		    monitorService.monitorBusyRate();
+        	  }
+        };
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(runnable, 0, 15000, TimeUnit.MILLISECONDS);
     }
 }
