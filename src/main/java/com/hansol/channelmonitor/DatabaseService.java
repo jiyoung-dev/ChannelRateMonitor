33b034093d074bbe75ced6749c00976ca5760dc5
jykim0317 <jykim0317@hansol.com>
2024-09-06 17:48:08 +0900

    센터별로 구분하도록 수정

--- src/main/java/com/hansol/channelmonitor/DatabaseService.java
+++ src/main/java/com/hansol/channelmonitor/DatabaseService.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -26,29 +28,63 @@ public class DatabaseService {
         }
     }
 
-    public int getBusyRate() {
-        int busyRate = 0;
+    public Map<String, Integer> getBusyRate() {
+    	  Map<String, Integer> busyRates = new HashMap<>();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         String currentDate = LocalDate.now().format(formatter);
 
-        String query = "SELECT TOP 1 busyrate FROM BusyRate WHERE RecordDate = ? ORDER BY RecordTime DESC";
-
+        String query = "WITH LastedBusyRate AS ( " +
+            "    SELECT " +
+            "        UniqueSystemID, " +
+            "        RecordDate, " +
+            "        RecordTime, " +
+            "        RealTotalPorts, " +
+            "        OccupiedCount, " +
+            "        SavingBorder, " +
+            "        BusyRate, " +
+            "        ChannelState, " +
+            "        ROW_NUMBER() OVER (PARTITION BY UniqueSystemID ORDER BY RecordTime DESC) AS rn " +
+            "    FROM BusyRate " +
+            "    WHERE UniqueSystemID IN ('00101001', '00101002', '00201001', '00201002') " +
+            "        AND RecordDate = ? " +
+            ") " +
+            "SELECT " +
+            "    CASE " +
+            "        WHEN UniqueSystemID IN ('00101001', '00101002') THEN '강남' " +
+            "        WHEN UniqueSystemID IN ('00201001', '00201002') THEN '송도' " +
+            "    END AS CenterName, " +
+            "    AVG(BusyRate) AS BusyRateAvg, " +
+            "    RecordDate, " +
+            "    RecordTime " +
+            "FROM LastedBusyRate " +
+            "WHERE rn = 2 " +  
+            "GROUP BY " +
+            "    CASE " +
+            "        WHEN UniqueSystemID IN ('00101001', '00101002') THEN '강남' " +
+            "        WHEN UniqueSystemID IN ('00201001', '00201002') THEN '송도' " +
+            "    END, " +
+            "    RecordDate, " +
+            "    RecordTime " +
+            "ORDER BY CenterName";
+        
         try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
              PreparedStatement stmt = conn.prepareStatement(query)) {
 
             stmt.setString(1, currentDate);
 
             try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    busyRate = rs.getInt("busyrate");
-                }
-            }
+              while (rs.next()) {
+                  String centerName = rs.getString("CenterName");
+                  int busyRateAvg = rs.getInt("BusyRateAvg");
+                  busyRates.put(centerName, busyRateAvg);
+              }
+          }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return busyRate;
+        return busyRates;
     }
 
     public String getCodeID(String codeName) {
--- src/main/java/com/hansol/channelmonitor/MonitorService.java
+++ src/main/java/com/hansol/channelmonitor/MonitorService.java
@@ -1,61 +1,94 @@
 package com.hansol.channelmonitor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MonitorService {
-    private static final Logger logger = LogManager.getLogger(MonitorService.class);
-    private DatabaseService dbService = new DatabaseService();
-    private int warningThreshold = 70;
-    private int dangerThreshold = 80;
-    private boolean warningLogged = false;
-    private boolean dangerLogged = false;
-
-    public void monitorBusyRate() {
-        try {
-            // 임계치 조회
-            getThresholds();
-
-            // BusyRate 조회
-            int currentBusyRate = dbService.getBusyRate();
-
-            if (currentBusyRate >= dangerThreshold) {
-                if (!dangerLogged) {
-                    logger.info("(위험) [THRESHOLD_EXCEEDED] Call Occupancy Rate: {}%, Threshold: {}%", currentBusyRate, dangerThreshold);
-                    dangerLogged = true;
-
-                    if (!warningLogged) {
-                        logger.info("(경고) [THRESHOLD_EXCEEDED] Call Occupancy Rate: {}%, Threshold: {}%", currentBusyRate, warningThreshold);
-                        warningLogged = true;
-                    }
-                }
-            } else if (currentBusyRate >= warningThreshold) {
-                if (!warningLogged) {
-                	logger.info("(경고) [THRESHOLD_EXCEEDED] Call Occupancy Rate: {}%, Threshold: {}%", currentBusyRate, warningThreshold);
-                    warningLogged = true;
-                }
-            } else {
-                // 현재 상태가 dangerThreshold 이하인 경우
-                if (dangerLogged && currentBusyRate < dangerThreshold) {
-                    logger.info("[CLEARED] 위험 알림이 해지되었습니다. Call Occupancy Rate: {}%, Threshold: {}%", currentBusyRate, dangerThreshold);
-                    dangerLogged = false;
-                }
-                if (warningLogged && currentBusyRate < warningThreshold) {
-                    logger.info("[CLEARED] 경고 알림이 해지되었습니다. Call Occupancy Rate: {}%, Threshold: {}%", currentBusyRate, warningThreshold);
-                    warningLogged = false;
-                }
-            }
-        } catch (Exception e) {
-            logger.error("모니터링 중 에러 발생: ", e);
-        }
-    }
-
-
-    private void getThresholds() {
-        String warningThresholdStr = dbService.getCodeID("위험");
-        String dangerThresholdStr = dbService.getCodeID("심각");
-
-        warningThreshold = Integer.parseInt(warningThresholdStr); // 70
-        dangerThreshold = Integer.parseInt(dangerThresholdStr);  // 80 
-    }
+	  private static final Logger logger = LogManager.getLogger(MonitorService.class);
+	  private DatabaseService dbService = new DatabaseService();
+	  private int warningThreshold = 70;
+	  private int dangerThreshold = 80;
+	
+	  // 센터별 상태를 관리
+	  private Map<String, Boolean> dangerLoggedMap = new HashMap<>();
+	  private Map<String, Boolean> warningLoggedMap = new HashMap<>();
+	
+	  public void monitorBusyRate() {
+	      try {
+	          // 임계치 조회
+	          getThresholds();
+	
+	          // BusyRate 조회
+	          Map<String, Integer> busyRates = dbService.getBusyRate();
+	
+	          System.out.println("busyRates : " + busyRates);
+	
+	          // 초기화는 처음 센터가 추가될 때만 수행
+	          for (String center : busyRates.keySet()) {
+	              dangerLoggedMap.putIfAbsent(center, false); // 최초 설정 시에만 false로 설정
+	              warningLoggedMap.putIfAbsent(center, false);
+	          }
+	
+	          for (Map.Entry<String, Integer> entry : busyRates.entrySet()) {
+	              String centerName = entry.getKey();
+	              int currentBusyRate = entry.getValue();
+	
+	              boolean dangerLogged = dangerLoggedMap.get(centerName);
+	              boolean warningLogged = warningLoggedMap.get(centerName);
+	
+	              // 위험 임계치 이상 
+	              if (currentBusyRate >= dangerThreshold) {
+	              	  if (!warningLogged) {
+	              		  // 경고 로그남김 (경고도 위험 포함임) 
+	              		  logger.info("[IVRA] {}_채널점유율: {}% 경고, 현재 채널점유율: {}%", centerName, warningThreshold, currentBusyRate);
+	              		  warningLoggedMap.put(centerName, true);
+	              	  }
+	                  if (!dangerLogged) {
+	                      logger.info("[IVRA] {}_채널점유율: {}% 위험, 현재 채널점유율: {}%", centerName, dangerThreshold, currentBusyRate);
+	                      dangerLoggedMap.put(centerName, true);  // 위험 로깅 상태 갱신
+	                  }
+	              } 
+	              // 경고 임계치 이상 & 위험 임계치 미만
+	              else if (currentBusyRate >= warningThreshold) {
+	                  if (dangerLogged) {
+	                      // 위험 해제 로그
+	                      logger.info("[IVRO] {}_채널점유율: {}% 위험 해제, 현재 채널점유율: {}%", centerName, dangerThreshold, currentBusyRate);
+	                      dangerLoggedMap.put(centerName, false);  // 위험 상태 해제
+	                  }
+	
+	                  if (!warningLogged) {
+	                      logger.info("[IVRA] {}_채널점유율: {}% 경고, 현재 채널점유율: {}%", centerName, warningThreshold, currentBusyRate);
+	                      warningLoggedMap.put(centerName, true);  // 경고 로깅 상태 갱신
+	                  }
+	              } 
+	              // 경고 임계치 미만(해제 상태)
+	              else {
+	                  // 위험 상태 해제 처리
+	                  if (dangerLogged) {
+	                      logger.info("[IVRO] {}_채널점유율: {}% 위험 해제, 현재 채널점유율: {}%", centerName, dangerThreshold, currentBusyRate);
+	                      dangerLoggedMap.put(centerName, false);  // 위험 상태 해제
+	                  }
+	                  // 경고 상태 해제 처리
+	                  if (warningLogged) {
+	                      logger.info("[IVRO] {}_채널점유율: {}% 경고 해제, 현재 채널점유율: {}%", centerName, warningThreshold, currentBusyRate);
+	                      warningLoggedMap.put(centerName, false);  // 경고 상태 해제
+	                  }
+	              }
+	          }
+	      } catch (Exception e) {
+	          logger.error("모니터링 중 에러 발생: ", e);
+	      }
+	  }
+	
+	  private void getThresholds() {
+	      String warningThresholdStr = dbService.getCodeID("위험");
+	      String dangerThresholdStr = dbService.getCodeID("심각");
+	
+	      warningThreshold = Integer.parseInt(warningThresholdStr); // 예: 70
+	      dangerThreshold = Integer.parseInt(dangerThresholdStr);  // 예: 80 
+	  }
 }
+
